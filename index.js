90f3e04db00a108450907a31a7b51ab1bcdd8cb1
Gabriel Lira <gabrielpaesdelira@gmail.com>
2024-05-28 00:30:56 -0300

    desafio do felipão concluído

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("salve, mundo!");
+let nomeHeroi = "Lior dos Piores";
+let xpHeroi = 60000;
+const msgSaida = "O herói de nome " + nomeHeroi + " está no nível de ";
+
+let listaNiveis = [
+    ["Ferro", 0, 1000],
+    ["Bronze", 1001, 2000],
+    ["Prata", 2001, 5000],
+    ["Ouro", 5001, 7000],
+    ["Platina", 7001, 8000],
+    ["Ascendente", 8001, 9000],
+    ["Imortal", 9001, 10000],
+    ["Radiante", 10001, Infinity]  // Corrigido para abranger todos os valores acima de 10001
+];
+
+for (let i = 0; i < listaNiveis.length; i++) {  // Condição de loop corrigida
+    if (xpHeroi >= listaNiveis[i][1] && xpHeroi <= listaNiveis[i][2]) {
+        console.log(msgSaida + listaNiveis[i][0]);
+        break;  // Adicionado para parar o loop após encontrar o nível correto
+    }
+}
